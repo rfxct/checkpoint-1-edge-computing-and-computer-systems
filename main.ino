713c25e20e794af4697c72953b08cb06433906f1
Marcos <xbrcvar@gmail.com>
2024-04-07 19:24:30 -0300

    Corrigir identação

--- main.ino
+++ main.ino
@@ -11,22 +11,22 @@ void setup()
     pinMode(LED_OK, OUTPUT);
     pinMode(LED_ALERTA, OUTPUT);
     pinMode(LED_PROBLEMA, OUTPUT);
-  
-	Serial.begin(9600);
+
+	  Serial.begin(9600);
 }
 
 void loop()
 {
   int luminosidade = analogRead(LDR_SENSOR);
   // MIN: 6 - MAX: 679
-    
+
   Serial.println(luminosidade);
-  
+
   // 6 - 200
   if (luminosidade < 200) {
     // LED OK
     digitalWrite(LED_OK, HIGH);
-    
+
     // RESET
     digitalWrite(LED_ALERTA, LOW);
     digitalWrite(LED_PROBLEMA, LOW);
@@ -34,26 +34,24 @@ void loop()
   } else if (luminosidade < 450) {
     // LED Alerta
     digitalWrite(LED_ALERTA, HIGH);
-    
+
     // RESET
     digitalWrite(LED_OK, LOW);
     digitalWrite(LED_PROBLEMA, LOW);
-    
+
     // Tocar buzina durante 3 segundos
     digitalWrite(BUZZER, HIGH);
     delay(3000);
-    
+
     // Parar buzina por 2 segundos depois de tocar
     digitalWrite(BUZZER, LOW);
     delay(2000);
   // 400 - 679
   } else {
     // LED Problema
-  	digitalWrite(LED_PROBLEMA, HIGH);
-    
+	  digitalWrite(LED_PROBLEMA, HIGH);
+
     // RESET
     digitalWrite(LED_OK, LOW);
     digitalWrite(LED_ALERTA, LOW);
   }
-  
-}
